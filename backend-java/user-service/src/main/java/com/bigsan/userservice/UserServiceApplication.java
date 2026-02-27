f1ce0288ea3eb86c3ef1229d650ffcf51034ede3
gyanalpanda <gyanalpanda@gmail.com>
2026-02-27 15:50:05 +0530

    Update UserServiceApplication.java

--- backend-java/user-service/src/main/java/com/bigsan/userservice/UserServiceApplication.java
+++ backend-java/user-service/src/main/java/com/bigsan/userservice/UserServiceApplication.java
@@ -1,1 +1,12 @@
+package com.bigsan.userservice;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
